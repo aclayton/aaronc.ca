ce7210f2a5648eb693c0c24e1a5688f41028d9e5
Aaron Clayton <aaronc@protonmail.ch>
2018-11-14 19:54:37 -0500

    contact off

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,7 +2,7 @@
   <nav>
     <router-link to="/">Portfolio</router-link><br />
     <router-link to="/about">About</router-link><br />
-    <router-link to="/contact">Contact</router-link>
+    <!-- <router-link to="/contact">Contact</router-link> -->
   </nav>
 </template>
 
